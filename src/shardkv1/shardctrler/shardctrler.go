3987fd1d5013a2dfd6737f452c5233bf8eec1b00
Abhishek Mandingi <mabhi256@gmail.com>
2026-02-12 18:00:14 +0530

    LAB-5B handle failed controller

--- src/shardkv1/shardctrler/shardctrler.go
+++ src/shardkv1/shardctrler/shardctrler.go
@@ -38,6 +38,21 @@ func MakeShardCtrler(clnt *tester.Clnt) *ShardCtrler {
 // controller. In part A, this method doesn't need to do anything. In
 // B and C, this method implements recovery.
 func (sck *ShardCtrler) InitController() {
+	for {
+		old, _, err := sck.getCurrent()
+		if err != rpc.OK {
+			return
+		}
+		new, _, err := sck.getNext()
+		if err != rpc.OK {
+			return
+		}
+		if new == nil || new.Num <= old.Num {
+			return // Already caught up
+		}
+		// recover movement of shards
+		sck.ChangeConfigTo(new)
+	}
 }
 
 // Called once by the tester to supply the first configuration.  You
@@ -46,7 +61,7 @@ func (sck *ShardCtrler) InitController() {
 // pick the key to name the configuration.  The initial configuration
 // lists shardgrp shardcfg.Gid1 for all shards.
 func (sck *ShardCtrler) InitConfig(cfg *shardcfg.ShardConfig) {
-	sck.IKVClerk.Put("config", cfg.String(), 0)
+	sck.IKVClerk.Put("config/current", cfg.String(), 0)
 }
 
 // Called by the tester to ask the controller to change the
@@ -55,54 +70,112 @@ func (sck *ShardCtrler) InitConfig(cfg *shardcfg.ShardConfig) {
 // controller.
 func (sck *ShardCtrler) ChangeConfigTo(new *shardcfg.ShardConfig) {
 	for {
-		old := sck.Query()
-		if old.Num >= new.Num {
+		current, currVer, _ := sck.getCurrent()
+
+		// current config is up-to-date
+		if current.Num >= new.Num {
 			return
 		}
 
-		success := true
-		for i := range shardcfg.NShards {
-			shardId := shardcfg.Tshid(i)
-			oldGid, oldServers, oldExists := old.GidServers(shardId)
-			newGid, newServers, newExists := new.GidServers(shardId)
-			if !oldExists || !newExists {
-				success = false
-				break
-			}
-			if oldGid == newGid {
-				continue
-			}
-
-			oldClerk := shardgrp.MakeClerk(sck.clnt, oldServers)
-			shardState, err := oldClerk.FreezeShard(shardId, new.Num)
-			if err != rpc.OK {
-				success = false
-				break
-			}
-
-			newClerk := shardgrp.MakeClerk(sck.clnt, newServers)
-			err = newClerk.InstallShard(shardId, shardState, new.Num)
-			if err != rpc.OK {
-				success = false
-				break
-			}
-
-			err = oldClerk.DeleteShard(shardId, new.Num)
-			if err != rpc.OK {
-				success = false
-				break
-			}
+		_, nextVer, _ := sck.getNext()
+		if err := sck.saveNext(new, nextVer); err != rpc.OK {
+			time.Sleep(20 * time.Millisecond)
+			continue
 		}
 
-		if success {
-			sck.IKVClerk.Put("config", new.String(), rpc.Tversion(old.Num))
+		if !sck.moveShards(current, new) {
+			time.Sleep(20 * time.Millisecond)
+			continue
 		}
-		time.Sleep(100 * time.Millisecond)
+
+		if err := sck.saveCurrent(new, currVer); err != rpc.OK {
+			time.Sleep(20 * time.Millisecond)
+			continue
+		}
+
+		sck.clearNext(nextVer + 1)
+		return
 	}
 }
 
 // Return the current configuration
 func (sck *ShardCtrler) Query() *shardcfg.ShardConfig {
-	cfgStr, _, _ := sck.IKVClerk.Get("config")
-	return shardcfg.FromString(cfgStr)
+	curr, _, _ := sck.getCurrent()
+	return curr
+}
+
+func (sck *ShardCtrler) getConfig(key string) (*shardcfg.ShardConfig, rpc.Tversion, rpc.Err) {
+	cfgStr, ver, err := sck.IKVClerk.Get(key)
+	if err == rpc.ErrNoKey || cfgStr == "" {
+		return nil, ver, rpc.OK
+	}
+	if err != rpc.OK {
+		return nil, ver, err
+	}
+	return shardcfg.FromString(cfgStr), ver, err
+}
+
+func (sck *ShardCtrler) getCurrent() (*shardcfg.ShardConfig, rpc.Tversion, rpc.Err) {
+	return sck.getConfig("config/current")
+}
+
+func (sck *ShardCtrler) getNext() (*shardcfg.ShardConfig, rpc.Tversion, rpc.Err) {
+	return sck.getConfig("config/next")
+}
+
+func (sck *ShardCtrler) setConfig(key, value string, ver rpc.Tversion) rpc.Err {
+	err := sck.Put(key, value, ver)
+
+	if err == rpc.ErrMaybe {
+		val, _, err := sck.Get(key)
+		if err != rpc.OK {
+			return err
+		}
+		if val == value {
+			return rpc.OK
+		}
+		return rpc.ErrVersion
+	}
+	return err
+}
+
+func (sck *ShardCtrler) saveCurrent(cfg *shardcfg.ShardConfig, ver rpc.Tversion) rpc.Err {
+	return sck.setConfig("config/current", cfg.String(), ver)
+}
+
+func (sck *ShardCtrler) saveNext(cfg *shardcfg.ShardConfig, ver rpc.Tversion) rpc.Err {
+	return sck.setConfig("config/next", cfg.String(), ver)
+}
+
+func (sck *ShardCtrler) clearNext(ver rpc.Tversion) {
+	sck.setConfig("config/next", "", ver)
+}
+
+func (sck *ShardCtrler) moveShards(old, new *shardcfg.ShardConfig) bool {
+	for i := range shardcfg.NShards {
+		shardId := shardcfg.Tshid(i)
+		oldGid, oldServers, _ := old.GidServers(shardId)
+		newGid, newServers, _ := new.GidServers(shardId)
+		if oldGid == newGid {
+			continue
+		}
+
+		oldClerk := shardgrp.MakeClerk(sck.clnt, oldServers)
+		shardState, err := oldClerk.FreezeShard(shardId, new.Num)
+		if err != rpc.OK {
+			return false
+		}
+
+		newClerk := shardgrp.MakeClerk(sck.clnt, newServers)
+		err = newClerk.InstallShard(shardId, shardState, new.Num)
+		if err != rpc.OK {
+			return false
+		}
+
+		err = oldClerk.DeleteShard(shardId, new.Num)
+		if err != rpc.OK {
+			return false
+		}
+	}
+	return true
 }
